ea773505a7c864939a24cb296725e1eca8fbed30
okunoryo <okuno.ryo.411@gmail.com>
2015-01-22 16:58:47 +0900

    ミスコンイメージURLゲット出来た

--- ml_my/miss_imgCrawl.py
+++ ml_my/miss_imgCrawl.py
@@ -15,18 +15,19 @@ def getLink():
     for list_url in data.findAll('li'):
         T = list_url.a
         links = url + T.get("href")
-        link_data.append(links)
-        print links
-    print link_data
-    return link_data
+        #link_data.append(links)
+        #print links
+        getImageUrl(links)
+#    print link_data
+    #return link_data
 
-def getImageUrl():
-    url = "http://misscolle.com/misskeio2014"
+def getImageUrl(link):
+    url = link
     html =urllib.urlopen(url).read()
     soup = BeautifulSoup.BeautifulSoup(html)
     data = soup.find(id="contest_key_visual")
     for img_url in data.findAll('img'):
         print url[:20] + img_url.get('src')
 
-getImageUrl()
-#getLink()
+#getImageUrl()
+getLink()
